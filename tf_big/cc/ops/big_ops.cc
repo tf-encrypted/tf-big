3ef03637026c8441e826384d9fcf36f76b0dfca8
Morten Dahl <mortendahlcs@gmail.com>
2019-07-08 17:05:58 +0200

    shape functions

--- tf_big/cc/ops/big_ops.cc
+++ tf_big/cc/ops/big_ops.cc
@@ -1,38 +1,70 @@
 #include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/common_shape_fns.h"
 #include "tensorflow/core/framework/shape_inference.h"
 
 REGISTER_OP("BigImport")
     .Attr("dtype: {int32, string}")
     .Input("in: dtype")
     .Output("val: variant")
-    .SetIsStateful();
+    .SetIsStateful()
+    .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
+        ::tensorflow::shape_inference::ShapeHandle output;
+        TF_RETURN_IF_ERROR(c->WithRankAtMost(c->input(0), 2, &output));
+        c->set_output(0, output);
+        return ::tensorflow::Status::OK();
+    });
 
 REGISTER_OP("BigExport")
     .Attr("dtype: {int32, string}")
     .Input("val: variant")
     .Output("out: dtype")
-    .SetIsStateful();
+    .SetIsStateful()
+    .SetShapeFn(::tensorflow::shape_inference::UnchangedShape);
 
 REGISTER_OP("BigAdd")
+    .Input("val0: variant")
     .Input("val1: variant")
-    .Input("val2: variant")
     .Output("res: variant")
-    .SetIsStateful();
+    .SetIsStateful()
+    .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
+        ::tensorflow::shape_inference::ShapeHandle val0 = c->input(0);
+        ::tensorflow::shape_inference::ShapeHandle val1 = c->input(1);
+        ::tensorflow::shape_inference::ShapeHandle res;
+        TF_RETURN_IF_ERROR(c->Merge(val0, val1, &res));
+        c->set_output(0, res);
+        return ::tensorflow::Status::OK();
+    });
 
 REGISTER_OP("BigSub")
+    .Input("val0: variant")
     .Input("val1: variant")
-    .Input("val2: variant")
     .Output("res: variant")
-    .SetIsStateful();
+    .SetIsStateful()
+    .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
+        ::tensorflow::shape_inference::ShapeHandle val0 = c->input(0);
+        ::tensorflow::shape_inference::ShapeHandle val1 = c->input(1);
+        ::tensorflow::shape_inference::ShapeHandle res;
+        TF_RETURN_IF_ERROR(c->Merge(val0, val1, &res));
+        c->set_output(0, res);
+        return ::tensorflow::Status::OK();
+    });
 
 REGISTER_OP("BigMul")
+    .Input("val0: variant")
     .Input("val1: variant")
-    .Input("val2: variant")
     .Output("res: variant")
-    .SetIsStateful();
+    .SetIsStateful()
+    .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
+        ::tensorflow::shape_inference::ShapeHandle val0 = c->input(0);
+        ::tensorflow::shape_inference::ShapeHandle val1 = c->input(1);
+        ::tensorflow::shape_inference::ShapeHandle res;
+        TF_RETURN_IF_ERROR(c->Merge(val0, val1, &res));
+        c->set_output(0, res);
+        return ::tensorflow::Status::OK();
+    });
 
 REGISTER_OP("BigMatMul")
+    .Input("val0: variant")
     .Input("val1: variant")
-    .Input("val2: variant")
     .Output("res: variant")
     .SetIsStateful();
