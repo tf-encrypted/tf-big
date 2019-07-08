6598d712f0d6d5add7f73d64f38f95b9a591605b
Morten Dahl <mortendahlcs@gmail.com>
2019-07-08 17:08:18 +0200

    convertion

--- tf_big/__init__.py
+++ tf_big/__init__.py
@@ -1,7 +1,13 @@
 # from __future__ import absolute_import
 
+from tf_big.python.convert import constant
+from tf_big.python.convert import convert_from_tensor
+from tf_big.python.convert import convert_to_tensor
 from tf_big.python.tensor import Tensor
 
 __all__ = [
+  'constant',
+  'convert_from_tensor',
+  'convert_to_tensor',
   'Tensor',
 ]
