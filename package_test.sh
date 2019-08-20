fdecf75073106d96bdb6d7f3b7fa49159144c2ab
Morten Dahl <mortendahlcs@gmail.com>
2019-08-20 15:45:17 +0200

    ..

--- package_test.sh
+++
@@ -1,9 +0,0 @@
-# This is the script overall responsible for testing the pip package against
-# multiple versions of TensorFlow.
-
-set -e
-set -x
-
-pip install -U tensorflow==1.13.1 && ./package_test_run.sh
-pip install -U tensorflow==1.13.2 && ./package_test_run.sh
-pip install -U tensorflow==1.14.0 && ./package_test_run.sh
