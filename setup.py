962de47a58342bdf86f69357fc5df39f8154b14e
Morten Dahl <mortendahlcs@gmail.com>
2019-08-28 17:47:23 +0200

    [release] 0.1.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tf-big",
-    version="0.1.1",
+    version="0.1.2",
     packages=setuptools.find_packages(),
     package_data={
         '': ['*.so'],
