4bca166e964a1d0ddff535028d69878788f8dd36
Morten Dahl <mortendahlcs@gmail.com>
2019-09-02 18:35:31 +0200

    decryption

--- examples/paillier.py
+++ examples/paillier.py
@@ -5,16 +5,27 @@ import tf_big
 # use secure operations by default
 tf_big.set_secure_default(True)
 
+
 class EncryptionKey:
-  def __init__(self, n, nn, g):
+  def __init__(self, n):
     self.n = n
-    self.nn = nn
-    self.g = g
+    self.nn = n * n
+    self.g = n + 1
+
 
 class DecryptionKey:
-  def __init__(self):
-    # TODO what do we need?
-    pass
+  def __init__(self, p, q):
+    n = p * q
+
+    self.n = n
+    self.nn = n * n
+    self.g = n + 1
+
+    order_of_n = (p - 1) * (q - 1)
+    self.d1 = order_of_n
+    self.d2 = tf_big.inv(order_of_n, n)
+    self.e = tf_big.inv(n, order_of_n)
+
 
 def dummy_keygen():
   # TODO use fixed large primes  
@@ -22,16 +33,14 @@ def dummy_keygen():
   q = tf_big.constant([19])
   n = p * q
 
-  g = n + 1
-  nn = n * n
-
-  ek = EncryptionKey(n, nn, g)
-  dk = DecryptionKey()
+  ek = EncryptionKey(n)
+  dk = DecryptionKey(p, q)
   return ek, dk
 
+
 def encrypt(ek, x):
-  # r = tf_big.random.uniform(ek.n, shape=x.shape)
-  r = tf_big.convert_to_tensor(tf.constant([[123, 124], [125, 126]])) # TODO
+  r = tf_big.random_uniform(maxval=ek.n, shape=x.shape)
+  # r = tf_big.convert_to_tensor(tf.constant([[123, 124], [125, 126]])) # TODO
   assert r.shape == x.shape, "Shapes are not matching: {}, {}".format(r.shape, x.shape)
 
   gx = tf_big.pow(ek.g, x, ek.nn)
@@ -40,34 +49,37 @@ def encrypt(ek, x):
   return c
 
 def decrypt(dk, c):
-  # TODO what do we need?
-  pass
+  gxd = tf_big.pow(c, dk.d1, dk.nn)
+  xd = (gxd - 1) // dk.n
+  x = (xd * dk.d2) % dk.n
+  return x
 
-def add(ek, c1, c2):
-  c = c1 * c2 % ek.nn
-  return c
+# def add(ek, c1, c2):
+#   c = c1 * c2 % ek.nn
+#   return c
 
-def mul(ek, c1, x2):
-  c = tf_big.pow(c1, x2, ek.nn)
-  return c
+# def mul(ek, c1, x2):
+#   c = tf_big.pow(c1, x2, ek.nn)
+#   return c
 
 ek, dk = dummy_keygen()
 
-# TODO(Morten) relace with lin reg computation?
+# # TODO(Morten) relace with lin reg computation?
 
-x1 = tf.constant([[1, 2], [3, 4]])
+x1 = tf.constant([[5]])
 c1 = encrypt(ek, x1)
 
-x1 = tf.constant([[5, 6], [7, 8]])
-c2 = encrypt(ek, x2)
+# x2 = tf.constant([[5, 6], [7, 8]])
+# c2 = encrypt(ek, x2)
 
-c3 = add(ek, c1, c2)
+# c3 = add(ek, c1, c2)
 
-c4 = mul(ek, c3, tf.constant(3))
+# c4 = mul(ek, c3, tf.constant(3))
 
-y = decrypt(dk, c4)
+y = decrypt(dk, c1)
 
 with tf.Session() as sess:
-  actual = sess.run(y)
-  expected = (x1 + x2) * 3
-  np.testing.assert_array_equal(actual, expected)
+  actual = sess.run(c1, y)
+  print(actual)
+  # expected = (x1 + x2) * 3
+  # np.testing.assert_array_equal(actual, expected)
